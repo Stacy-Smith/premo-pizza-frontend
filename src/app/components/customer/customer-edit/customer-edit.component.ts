1bd00d820dee6c1c46ccb62ca471ae4314d1d9fe
kjdowns <kevin.downs04@gmail.com>
2021-04-20 17:01:22 -0400

    customer component family now fully functional

--- src/app/components/customer/customer-edit/customer-edit.component.ts
+++ src/app/components/customer/customer-edit/customer-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from 'src/app/models/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-customer-edit',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerEditComponent implements OnInit {
 
-  constructor() { }
+  currentCustomer: Customer = null;
+  message: string = ''
+
+  constructor(private customerService: CustomerService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getCustomer(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getCustomer(id): void {
+    this.customerService.get(id)
+      .subscribe(data => {
+        this.currentCustomer = data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  updateCustomer(): void {
+    this.customerService.update(this.currentCustomer.id, this.currentCustomer)
+      .subscribe( response => {
+        console.log(response);
+        this.message = 'The customer was updated successfully!';
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  deleteCustomer(): void {
+    this.customerService.delete(this.currentCustomer.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/customers']);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -1,6 +1,6 @@
 
 export class Customer {
-    customerId?: number;
+    id?: number;
     name: string;
     address: string;
     phoneNumber: number;
