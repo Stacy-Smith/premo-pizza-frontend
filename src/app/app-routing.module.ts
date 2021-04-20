af5d3a2c0b6dac7841e292180df31833acfb423f
kjdowns <kevin.downs04@gmail.com>
2021-04-20 16:44:32 -0400

    add routing for customers-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerAddComponent } from './components/customer/customer-add/customer-add.component';
+import { CustomerEditComponent } from './components/customer/customer-edit/customer-edit.component';
 import { CustomerListComponent } from './components/customer/customer-list/customer-list.component';
 import { EmployeeAddComponent } from './components/employee/employee-add/employee-add.component';
 import { EmployeeUpdateComponent } from './components/employee/employee-update/employee-update.component';
@@ -9,6 +10,7 @@ import { EmployeeComponent } from './components/employee/employee.component';
 const routes: Routes = [
   { path: 'customers', component: CustomerListComponent },
   { path: 'customers/add', component: CustomerAddComponent },
+  { path: 'customers/:id', component: CustomerEditComponent},
   { path: 'employees', component: EmployeeComponent },
   { path: 'employees/add', component: EmployeeAddComponent },
   { path: 'employees/:id', component: EmployeeUpdateComponent}
