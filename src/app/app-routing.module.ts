2177e945c52bf23922005ee6cee3a4c0219fa480
kjdowns <kevin.downs04@gmail.com>
2021-04-21 12:17:32 -0400

    set up data flow to orders-add component as well as routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,17 +7,26 @@ import { CustomerListComponent } from './components/customer/customer-list/custo
 import { EmployeeAddComponent } from './components/employee/employee-add/employee-add.component';
 import { EmployeeUpdateComponent } from './components/employee/employee-update/employee-update.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { OrdersAddComponent } from './components/orders/orders-add/orders-add.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
+  // Customer Routes
   { path: 'customers', component: CustomerListComponent },
   { path: 'customers/add', component: CustomerAddComponent },
   { path: 'customers/:id', component: CustomerEditComponent},
+
+  // Employee Routes
   { path: 'employees', component: EmployeeComponent },
   { path: 'employees/add', component: EmployeeAddComponent },
   { path: 'employees/:id', component: EmployeeUpdateComponent},
+
+  // Orders Routes
   { path: 'orders', component: OrdersComponent },
+  { path: 'orders/add', component: OrdersAddComponent },
+
+  // Product routes
   { path: 'products', component: ProductComponent }
 ];
 
