46b61aeaa2c860e131ca5baa3edb88aa7703eb7c
kjdowns <kevin.downs04@gmail.com>
2021-04-22 16:35:58 -0400

    add functionality to view order details

--- src/app/components/orders/order-detail/order-detail.component.ts
+++ src/app/components/orders/order-detail/order-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderProduct } from 'src/app/models/orderproduct';
+import { Order } from 'src/app/models/orders';
+import { OrderproductService } from 'src/app/services/orderproduct.service';
+import { OrderService } from 'src/app/services/orders.service';
 
 @Component({
   selector: 'app-order-detail',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailComponent implements OnInit {
 
-  constructor() { }
+  order: Order;
+  orderProducts: OrderProduct[] = [];
+
+  constructor(private orderService: OrderService,
+              private orderProductService: OrderproductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getOrder(this.route.snapshot.paramMap.get('id'));
+    this.retrieveOrderProducts();
+  }
+
+  retrieveOrderProducts(): void {
+    this.orderProductService.getAll()
+      .subscribe(data => {
+        data.forEach(orderProd => {
+          if(this.order.orderId == orderProd.order.orderId){
+            this.orderProducts.push(orderProd);
+          }
+        });
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  getOrder(id): void {
+    this.orderService.get(id)
+      .subscribe(data => {
+        this.order = data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
