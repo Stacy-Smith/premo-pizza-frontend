1c77b994a47c41579d1b7abc9bf57204924f4383
kjdowns <kevin.downs04@gmail.com>
2021-04-23 17:08:33 -0400

    test

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8081/api/customers';
+const baseUrl = 'http://localhost:8080/api/customers';
 
 @Injectable({
   providedIn: 'root'
